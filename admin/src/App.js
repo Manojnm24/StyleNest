3e1fe81953cdeea9aee96e24173b6dcfc6774efd
Manojnm24 <manoj80204@gmail.com>
2025-12-26 23:25:00 +0530

    Updated the admin app to run in both local and netlify

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,8 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
+//Did this changes to run in both netlify and local
+export const backend_url = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
 export const currency = '₹';
 
 function App() {
